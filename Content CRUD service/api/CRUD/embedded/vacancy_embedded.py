ad6071701e19e1706e381c03bbff2024fb805bf4
murrchacho <murrchacho@gmail.com>
2022-10-19 01:16:05 +0500

    Начата работа над микросервисом CRUD. ToDo: исправить добавление данных в словари salary и required_skills

---
+++ Content CRUD service/api/CRUD/embedded/vacancy_embedded.py
@@ -0,0 +1,93 @@
+from dataclasses import fields
+from datetime import datetime
+from email.policy import default
+from posixpath import abspath
+from djongo import models
+
+
+
+
+class TechStackEmbedded(models.Model):
+    name = models.CharField(max_length=50,  default="")
+    experiance = models.CharField(max_length=100,  default="")
+    
+    class Meta:
+        abstract=True
+
+
+class SalaryRangeEmbedded(models.Model):
+    frm = models.IntegerField(default=0),
+    to = models.IntegerField(default=0)
+
+    class Meta:
+        abstract=True
+
+class SalaryEmbedded(models.Model):
+    salary_level = models.EmbeddedField(
+        model_container = SalaryRangeEmbedded
+    )
+    currency = models.CharField(max_length=3,  default='RUB')
+
+    class Meta:
+        abstract=True
+
+
+class PositionEmbedded(models.Model):
+    title = models.CharField(max_length=50, default="")
+    level = models.CharField(max_length=20, default="")
+
+    class Meta:
+        abstract=True
+
+
+class MetaEmbedded(models.Model):
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+
+    class Meta:
+        abstract=True
+
+
+class CommonScheduleEmbedded(models.Model):
+    type = models.CharField(max_length=50, default="")
+    schedule = models.CharField(max_length=50, default="")
+
+    class Meta:
+        abstract=True
+
+
+class WorkScheduleEmbedded(models.Model):
+    in_person_schedule = models.EmbeddedField(
+        model_container = CommonScheduleEmbedded
+    )
+    remote_schedule = models.EmbeddedField(
+        model_container = CommonScheduleEmbedded
+    )
+
+    class Meta:
+        abstract=True
+
+
+class LocationEmbedded(models.Model):
+    country = models.CharField(max_length=50, default="")
+    city = models.CharField(max_length=50, default="")
+    address = models.CharField(max_length=100, default="")
+
+    class Meta:
+        abstract=True
+
+
+class InterviewEmbedded(models.Model):
+    format = models.CharField(max_length=50, default="")
+    stages = models.IntegerField(default=0)
+
+    class Meta:
+        abstract=True
+
+
+class SkillsEmbedded(models.Model):
+    skill = models.CharField(max_length=50, default="")
+    level = models.CharField(max_length=50, default="")
+
+    class Meta:
+        abstract=True
